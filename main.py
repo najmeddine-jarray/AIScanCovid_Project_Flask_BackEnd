d82161738b53e2643dc5fbd5ff05cf20eb4ae445
Najmeddine <najmejarray@gmail.com>
2025-04-10 18:51:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+
 import psycopg2
 from flask import Flask, jsonify, request, send_file
 from psycopg2 import connect, extras
@@ -46,10 +49,10 @@ port = environ.get('DB_PORT')
 
 
 def get_db_connection():
-    conn = connect(host='localhost',
-                   database='scancovid',
-                   user='postgres',
-                   password='password',
+    conn = connect(host='dpg-cvrupa0gjchc73a3plg0-a.oregon-postgres.render.com',
+                   database='ai_covid_scan',
+                   user='ai_covid_scan_user',
+                   password='cJDLzYKDo7RCl7ueWJvrpCdaJUfH7NZ4',
                    port=5432)
 
     return conn
@@ -60,19 +63,48 @@ def generate_random_string(length=8):
     return ''.join(random.choice(letters_and_digits) for _ in range(length))
 
 
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    email = data['email']
+    password = data['password']
+
+    # Hash the password
+    hashed_password = generate_password_hash(password)
+
+    # Check if email already exists
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    cur.execute("SELECT * FROM admin_login WHERE email = %s", (email,))
+    existing_user = cur.fetchone()
+
+    if existing_user:
+        return jsonify({'message': 'Email already exists!'}), 400
+
+    # Insert new user into the database
+    cur.execute("INSERT INTO admin_login (email, password) VALUES (%s, %s)",
+                (email, hashed_password))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return jsonify({'message': 'User registered successfully!'}), 201
+
+
 @app.route('/login', methods=['POST'])
 def login():
     data = request.json
     email = data['email']
     password = data['password']
+
     conn = get_db_connection()
     cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-    cur.execute("SELECT * FROM admin_login")
-    users = cur.fetchall()
-    for user in users:
-        if user['email'] == email and user['password'] == password:
-            return jsonify({'message': 'Login Successful. '}),  200
-    return jsonify({'message': 'Email Not already exists in the database. '}), 400
+    cur.execute("SELECT * FROM admin_login WHERE email = %s", (email,))
+    user = cur.fetchone()
+
+    if user and check_password_hash(user['password'], password):
+        return jsonify({'message': 'Login Successful'}), 200
+    return jsonify({'message': 'Invalid email or password'}), 400
 
 
 @app.route('/get', methods=['GET'])
@@ -155,7 +187,46 @@ def scanner():
         return jsonify({"message": "An error occurred while processing the request."}), 500
 
 
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    name = request.form['Name']
+    im = request.files['images']
+
+    im.save('./static/images/{}'.format(im.filename))
+    im = cv2.imread('./static/images/{}'.format(im.filename))
+    im = np.asarray(im)
+    ima = cv2.resize(im, (160, 160))
+    ima = ima.reshape(1, 160, 160, 3)
+    # im = load_img(im, target_size=(160, 160))
+    # im = np.expand_dims(im, axis=0)
+    prediction = model.predict(ima)
+    class_predictions = np.argmax(prediction, axis=1)
+    print(class_predictions)
+    if class_predictions == 0:
+        pred = 'Covid'
+    elif class_predictions == 1:
+        pred = 'Normal'
+    elif class_predictions == 2:
+        pred = 'Viral Pneumonia'
+    message = pred
+    # con = sqlite3.connect('sql_pr.sqlite3')
+    # cur = con.cursor()
+    # cur.execute("INSERT INTO classification (name, xray,prediction) values ('{}','{}','{}')".format(
+    #     name, im, message))
+    # print('added successfully to database !! ')
+    # con.commit()
+    # con.close()
+    return message
+
+
 @app.route('/', methods=['GET'])
 def home():
 
     return jsonify('AI ScanCovid Server Application')
+
+
+if __name__ == '__main__':
+    print("welcome")
+    # port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=5000)
+    # app.run()
