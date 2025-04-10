d1a72e4c940b53ffc8c5841019ba727a95623f8a
Najmeddine <najmejarray@gmail.com>
2025-04-10 19:21:21 +0100

    Update and rename run.py to app.py

--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-from main import app
-if __name__ == '__main__':
-    print("welcome ")
-    # port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', debug=True)
-    # app.run()
