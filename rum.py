b693ed46d6f9fb9a87d1e1a6b43d44639ccaaf79
Najmeddine <najmejarray@gmail.com>
2025-04-10 19:33:17 +0100

    Update and rename app.py to rum.py

--- rum.py
+++ rum.py
@@ -4,4 +4,4 @@ from app import app  # Import from the app package
 
 if __name__ == '__main__':
     print("Starting AI ScanCovid Server...")
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run()
