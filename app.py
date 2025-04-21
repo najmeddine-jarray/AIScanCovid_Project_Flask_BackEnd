dec8c4445157463d8128e97e492737dafaab86d8
Najmeddine <najmejarray@gmail.com>
2025-04-21 20:43:59 +0100

    Update and rename run.py to app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+# run.py
+import os
+from main import app  # Import from the app package
+
+if __name__ == '__main__':
+    print("Starting AI ScanCovid Server...")
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
--- run.py
+++
@@ -1,7 +0,0 @@
-# run.py
-from main import app 
-
-if __name__ == '__main__':
-
-    print("Starting AI ScanCovid Server...")
-    app.run()
